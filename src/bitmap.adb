01dc719106655b170565f4a97aefd56eb9ab3918
emmanuelrs <emmanuelrs@gmail.com>
2015-09-05 17:45:28 -0600

    binary function ready

--- src/bitmap.adb
+++ src/bitmap.adb
@@ -10,11 +10,14 @@ with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 with Ada.Strings.Unbounded.Text_IO; use Ada.Strings.Unbounded.Text_IO;
 
 procedure Bitmap is
+
+   -- Renombra paquetes para usarlos más fácilmente.
    package IIO is new Ada.Text_IO.Integer_IO(Integer);
    package SU renames Ada.Strings.Unbounded;
-   function To_Binary(N: Natural) return SU.Unbounded_String is
+
+   function DecToBin(N: Natural) return SU.Unbounded_String is
       use type SU.Unbounded_String;
-      S: String(1 .. 1024);
+      S: String(1 .. 1026);
       Left:  Positive := S'First;
       Right: Positive := S'Last;
       Result : SU.Unbounded_String;
@@ -28,13 +31,13 @@ procedure Bitmap is
       end loop;
       Result := SU.To_Unbounded_String(S(Left+1 .. Right-1));
       return Result;
-   end To_Binary;
+   end DecToBin;
 
    Number : Natural := 0;
    Result : SU.Unbounded_String := SU.Null_Unbounded_String;
 begin
    Put("Write a number: ");
    Get(Number);
-   Result := To_Binary(Number);
+   Result := DecToBin(Number);
    Put(Result);
 end Bitmap;
